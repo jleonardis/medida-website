5238147155b44efc496d899c54fdc83900ac023f
jleonardis <jakeleonardis@gmail.com>
2024-10-29 20:45:10 -0600

    should no longer show language strings before they've loaded

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,8 +6,9 @@
 	import { FontAwesomeIcon } from '@fortawesome/svelte-fontawesome';
 	// src/lib/i18n/index.ts
 	import { browser } from '$app/environment';
-	import { init, register, locale, _ } from 'svelte-i18n';
+	import { init, register, locale, _, waitLocale } from 'svelte-i18n';
 	import { scheduleLink, whatsappLink, facebookLink, emailLink, linkedinLink } from '$lib/links';
+    import { onMount } from 'svelte';
 
 
 	const defaultLocale = 'es'
@@ -34,87 +35,95 @@
 	const closeMenu = () => {
 		isOpen = false;
 	};
+
+	let loading = $state(true);
+	onMount(async () => {
+		await waitLocale();
+		loading = false;
+	});
 </script>
 
-<!-- Navbar -->
-<nav class="flex flex-row items-center justify-between p-4 md:px-10 md:py-4">
-	<div class="absolute block md:hidden top-0 right-2">
-		{#if $locale === 'es'}
-			<span onclick={() => changeLanguage('en')} class="text-gray-700 hover:text-primary-blue text-xs cursor-pointer">English</span>
-		{:else}
-			<span onclick={() => changeLanguage('es')} class="text-gray-700 hover:text-primary-blue text-xs cursor-pointer">Español</span>
-		{/if}
-	</div>
-	<div><a href="/"><img class="h-14 mt-4 md:mt-2" src="/images/logo.png" /></a></div>
-	<!-- Hamburger Button -->
-	<button
-		class="mt-6 flex flex-col items-center justify-center w-8 h-8 space-y-1 md:hidden"
-		onclick={() => (isOpen = true)}
-		>
-		<div class="w-8 h-1 bg-gray-800 transition-transform duration-300" class:hamburger-top-line={isOpen}></div>
-		<div class="w-8 h-1 bg-gray-800 transition-opacity duration-300" class:hamburger-middle-line={isOpen}></div>
-		<div class="w-8 h-1 bg-gray-800 transition-transform duration-300" class:hamburger-bottom-line={isOpen}></div>
-	</button>
-
-	<!-- Mobile Menu -->
-	<div class="fixed inset-0 z-40 flex flex-col items-center justify-center bg-white md:hidden" class:hidden={!isOpen}>
-		<div class="absolute top-0 right-0 p-4">
-			<button
-				onclick={closeMenu}
-				class="text-2xl"
-				>
-				&times;
-			</button>
-		</div>
-		<a href="/how-we-work" onclick={closeMenu} class="text-2xl my-4">{$_('how_we_work')}</a>
-		<a href="/vision" onclick={closeMenu} class="text-2xl my-4">{$_('our_vision')}</a>
-		<div class="space-x-8 my-4">
-			<a href="{emailLink}" class="text-2xl my-4"><FontAwesomeIcon icon={faEnvelope} class="md:text-2xl text-gray-700" /></a>
-			<a href="{whatsappLink}" class="text-2xl my-4"><FontAwesomeIcon icon={faWhatsapp} class="text-green-500"/></a>
-		</div>
-		<a href="{scheduleLink}" class="px-6 py-2 my-4 bg-primary-blue text-white rounded-lg hover:bg-primary-orange">{$_('book_consultation')}</a>
-	</div>
-	<div class="md:flex flex-col hidden">
-		<div class="flex text-right ml-auto mb-1 items-center space-x-2">
+{#if !loading}
+	<!-- Navbar -->
+	<nav class="flex flex-row items-center justify-between p-4 md:px-10 md:py-4">
+		<div class="absolute block md:hidden top-0 right-2">
 			{#if $locale === 'es'}
 				<span onclick={() => changeLanguage('en')} class="text-gray-700 hover:text-primary-blue text-xs cursor-pointer">English</span>
 			{:else}
 				<span onclick={() => changeLanguage('es')} class="text-gray-700 hover:text-primary-blue text-xs cursor-pointer">Español</span>
 			{/if}
 		</div>
-		<div class="flex items-center space-x-4 my-4">
-			<ul class="flex space-x-8 md:text-xl mx-2 text-gray-700">
-				<li><a href="/how-we-work" class="hover:text-primary-blue">{$_('how_we_work')}</a></li>
-				<li><a href="/vision" class="hover:text-primary-blue mr-10">{$_('our_vision')}</a></li>
-			<li>
-				<a href="{emailLink}">
-					<FontAwesomeIcon icon={faEnvelope} class="md:text-2xl text-gray-700" />
-				</a>
-			</li>
-			<li>
-				<a href="{whatsappLink}" class="mr-10">
-					<FontAwesomeIcon icon={faWhatsapp} class="md:text-2xl text-green-500" />
-				</a>
-			</li>
-			</ul>
-			<a href="{scheduleLink}" class="px-6 py-2 bg-primary-blue text-white rounded-lg hover:bg-primary-orange">{$_('book_consultation')}</a>
+		<div><a href="/"><img class="h-14 mt-4 md:mt-2" src="/images/logo.png" /></a></div>
+		<!-- Hamburger Button -->
+		<button
+			class="mt-6 flex flex-col items-center justify-center w-8 h-8 space-y-1 md:hidden"
+			onclick={() => (isOpen = true)}
+			>
+			<div class="w-8 h-1 bg-gray-800 transition-transform duration-300" class:hamburger-top-line={isOpen}></div>
+			<div class="w-8 h-1 bg-gray-800 transition-opacity duration-300" class:hamburger-middle-line={isOpen}></div>
+			<div class="w-8 h-1 bg-gray-800 transition-transform duration-300" class:hamburger-bottom-line={isOpen}></div>
+		</button>
+
+		<!-- Mobile Menu -->
+		<div class="fixed inset-0 z-40 flex flex-col items-center justify-center bg-white md:hidden" class:hidden={!isOpen}>
+			<div class="absolute top-0 right-0 p-4">
+				<button
+					onclick={closeMenu}
+					class="text-2xl"
+					>
+					&times;
+				</button>
+			</div>
+			<a href="/how-we-work" onclick={closeMenu} class="text-2xl my-4">{$_('how_we_work')}</a>
+			<a href="/vision" onclick={closeMenu} class="text-2xl my-4">{$_('our_vision')}</a>
+			<div class="space-x-8 my-4">
+				<a href="{emailLink}" class="text-2xl my-4"><FontAwesomeIcon icon={faEnvelope} class="md:text-2xl text-gray-700" /></a>
+				<a href="{whatsappLink}" class="text-2xl my-4"><FontAwesomeIcon icon={faWhatsapp} class="text-green-500"/></a>
+			</div>
+			<a href="{scheduleLink}" class="px-6 py-2 my-4 bg-primary-blue text-white rounded-lg hover:bg-primary-orange">{$_('book_consultation')}</a>
 		</div>
-	</div>
-  </nav>
+		<div class="md:flex flex-col hidden">
+			<div class="flex text-right ml-auto mb-1 items-center space-x-2">
+				{#if $locale === 'es'}
+					<span onclick={() => changeLanguage('en')} class="text-gray-700 hover:text-primary-blue text-xs cursor-pointer">English</span>
+				{:else}
+					<span onclick={() => changeLanguage('es')} class="text-gray-700 hover:text-primary-blue text-xs cursor-pointer">Español</span>
+				{/if}
+			</div>
+			<div class="flex items-center space-x-4 my-4">
+				<ul class="flex space-x-8 md:text-xl mx-2 text-gray-700">
+					<li><a href="/how-we-work" class="hover:text-primary-blue">{$_('how_we_work')}</a></li>
+					<li><a href="/vision" class="hover:text-primary-blue mr-10">{$_('our_vision')}</a></li>
+				<li>
+					<a href="{emailLink}">
+						<FontAwesomeIcon icon={faEnvelope} class="md:text-2xl text-gray-700" />
+					</a>
+				</li>
+				<li>
+					<a href="{whatsappLink}" class="mr-10">
+						<FontAwesomeIcon icon={faWhatsapp} class="md:text-2xl text-green-500" />
+					</a>
+				</li>
+				</ul>
+				<a href="{scheduleLink}" class="px-6 py-2 bg-primary-blue text-white rounded-lg hover:bg-primary-orange">{$_('book_consultation')}</a>
+			</div>
+		</div>
+	</nav>
 
-  {@render children()}
+	{@render children()}
 
-  <!-- Footer -->
-  <footer class="bg-primary-blue text-white text-center py-8">
-	<div class="flex justify-center space-x-4">
-		<a href="{emailLink}" class="text-2xl"><FontAwesomeIcon icon={faEnvelope} /></a>
-		<a href="{whatsappLink}" class="text-2xl"><FontAwesomeIcon icon={faWhatsapp} /></a>
-		<a href="{facebookLink}" class="text-2xl"><FontAwesomeIcon icon={faFacebook} /></a>
-		<a href="{linkedinLink}" class="text-2xl"><FontAwesomeIcon icon={faLinkedin} /></a>
-	</div>
-	<p class="text-xs mt-4">{contactEmail}</p>
-	<p class="text-xs my-2">{whatsappNumber}</p>
-  </footer>
+	<!-- Footer -->
+	<footer class="bg-primary-blue text-white text-center py-8">
+		<div class="flex justify-center space-x-4">
+			<a href="{emailLink}" class="text-2xl"><FontAwesomeIcon icon={faEnvelope} /></a>
+			<a href="{whatsappLink}" class="text-2xl"><FontAwesomeIcon icon={faWhatsapp} /></a>
+			<a href="{facebookLink}" class="text-2xl"><FontAwesomeIcon icon={faFacebook} /></a>
+			<a href="{linkedinLink}" class="text-2xl"><FontAwesomeIcon icon={faLinkedin} /></a>
+		</div>
+		<p class="text-xs mt-4">{contactEmail}</p>
+		<p class="text-xs my-2">{whatsappNumber}</p>
+	</footer>
+{/if}
 
   <style>
 	hamburger-top-line {
