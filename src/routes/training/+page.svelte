0b276297c0d292bd2a2c9ad708541d6776b00f2f
jleonardis <jakeleonardis@gmail.com>
2024-10-31 09:18:42 -0600

    trying new layout on training page

--- src/routes/training/+page.svelte
+++ src/routes/training/+page.svelte
@@ -3,27 +3,27 @@
   
   </script>
   
-  <div class="flex flex-col items-center p-8">
-    <h1 class="text-4xl font-bold text-primary-blue text-center mb-8">{$_('training_title')}</h1>
-    <p class="text-xl text-gray-600 text-center mb-8">{$_('training_subtitle')}</p>
+  <div class="flex flex-col items-center">
+    <h1 class="text-4xl font-bold text-primary-blue text-center mt-24 mb-8">{$_('training_title')}</h1>
+    <p class="text-xl text-gray-600 text-center mb-24">{$_('training_subtitle')}</p>
     
     <!-- Section 1: Comprehensive training -->
-    <section class="md:px-16 py-8 w-full max-w-7xl flex flex-col md:flex-row items-center justify-between">
-      <div class="md:w-1/2 order-2 md:order-1 p-6">
+    <section class="mt-16 w-full flex flex-col md:flex-row justify-between">
+      <div class="md:w-1/2 order-2 md:order-1 p-24">
         <h2 class="text-3xl font-bold text-primary-blue mb-4">{$_('comprehensive_training_title')}</h2>
         <p class="text-xl text-gray-600 mb-10">{$_('comprehensive_training_subtitle')}</p>
       </div>
-      <div class="md:w-1/2 p-6 order-1 md:order-2">
+      <div class="md:w-1/2 order-1 md:order-2">
         <img src="/images/comprehensive-training.jpg" alt="Comprehensive Training" class="w-full h-auto object-cover">
       </div>
     </section>
   
     <!-- Section 2: Long-term support -->
-    <section class="md:px-16 py-8 w-full max-w-7xl flex flex-col md:flex-row items-center justify-between">
-      <div class="md:w-1/2 p-6">
+    <section class="mb-24 w-full flex flex-col md:flex-row justify-between">
+      <div class="md:w-1/2">
         <img src="/images/long-term-support.jpg" alt="Data Management" class="w-full h-auto object-cover">
       </div>
-      <div class="md:w-1/2 p-6">
+      <div class="md:w-1/2 p-24">
         <h2 class="text-3xl font-bold text-primary-blue mb-4">{$_('long_term_support_title')}</h2>
         <p class="text-xl text-gray-600 mb-10">{$_('long_term_support_subtitle')}</p>
         <!-- <ul class="text-lg text-gray-600 space-y-2">
