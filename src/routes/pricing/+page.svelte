ea0d38e3c23f92eceb57f309cf7ef1eac80ccf51
jleonardis <jakeleonardis@gmail.com>
2024-11-11 16:59:01 -0600

    mobile adjust

--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -5,12 +5,12 @@
     // Assuming translations have been set up correctly
 </script>
 
-<section class="w-screen px-24">
-    <div class="mt-24 mx-24 px-24 text-2xl leading-relaxed text-primary-blue text-center">
+<section class="w-screen md:px-24 p-5">
+    <div class="mt-10 md:mt-24 md:mx-24 md:px-24 text-2xl leading-relaxed text-primary-blue text-center">
         {$_('pricing_description')}
     </div>
 </section>
 
-<section class="my-2 md:py-16 text-center md:mb-24">
+<section class="my-10 md:py-16 text-center md:mb-24">
     <a href="{scheduleLink}" class="px-2 md:px-8 py-4 bg-primary-blue text-white text-xs md:text-base rounded-lg hover:bg-primary-orange">{$_('cta_simple_button_text')}</a>
   </section>
